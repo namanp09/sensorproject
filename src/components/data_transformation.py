3d5f15eb8ceafd01ee13e7811e9ab1daaf80e337
NAMAN <mac@Macs-MacBook-Air.local>
2025-01-13 18:53:28 +0530

    Fixed indentation error in data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -2,6 +2,8 @@ import sys
 import os
 import pandas as pd
 import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import RobustScaler, FunctionTransformer
@@ -11,82 +13,106 @@ from sklearn.preprocessing import StandardScaler
 from src.constant import *
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import MainUtils
+from src.utils.main_utils import MainUtils
 from dataclasses import dataclass
+
 
 @dataclass
 class DataTransformationConfig:
     artifact_dir = os.path.join(artifact_folder)
-    transformed_train_file_path = os.path.join(artifact_dir, "train.npy")
-    transformed_test_file_path = os.path.join(artifact_dir, "test.npy")
-    transformed_object_file_path = os.path.join(artifact_dir, "preprocessor.pkl")
+    transformed_train_file_path = os.path.join(artifact_dir,'train.npy')
+    transformed_test_file_path = os.path.join(artifact_dir,'test.npy')
+    transformed_object_file_path = os.path.join(artifact_dir,'preprocessor.pkl')
+
 
 class DataTransformation:
-    def __init__(self, feature_store_file_path):
+    def __init__(self,feature_store_file_path):
         self.feature_store_file_path = feature_store_file_path
-        self.data_transformation_config = DataTransformationConfig()
-        self.utils = MainUtils()
-        
-    @staticmethod
-    def get_data(feature_store_file_path: str)->pd.DataFrame:
-        
-        try:
-            data=pd.read_csv(feature_store_file_path)
-            data.rename(columns={"Good/Bad":TARGET_COLUMN},inplace=True)
-            return data
-        
-        except Exception as e:
-            raise CustomException(e,sys) 
-        
-    def get_data_transformer_object(self):
-        
-        try:
-            imputer_step = ('imputer', SimpleImputer(strategy="constant", fill_value=0))
-            scaler_step = ('scaler', RobustScaler())
 
-            preprocessor = Pipeline(
-                steps=[
-                    imputer_step,
-                    scaler_step
-                ]
+        self.data_transformation_config = DataTransformationConfig()
+
+        self.utils = MainUtils()
+
+    @staticmethod
+    def get_data(feature_store_file_path: str) ->pd.DataFrame:
+
+        try:
+
+            data = pd.read_csv(feature_store_file_path)
+
+            data.rename(columns={"Good/Bad": TARGET_COLUMN}, inplace=True)
+
+            return data
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def get_data_transformer_object(self, dataframe: pd.DataFrame):
+
+        try:
+        # Dynamically identify numeric and categorical columns
+            numeric_columns = dataframe.select_dtypes(include=['number']).columns.tolist()
+            categorical_columns = dataframe.select_dtypes(exclude=['number']).columns.tolist()
+
+        # Define transformation steps for numeric and categorical data
+            numeric_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='constant', fill_value=0)),
+            ('scaler', RobustScaler())
+            ])
+
+            categorical_transformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('onehot', OneHotEncoder(handle_unknown='ignore'))
+            ])
+
+        # Create ColumnTransformer
+            preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, numeric_columns),
+                ('cat', categorical_transformer, categorical_columns)
+            ]
             )
 
             return preprocessor
         
         except Exception as e:
-            raise CustomException(e,sys)
-        
+            raise CustomException(e, sys)
+
+    
     def initiate_data_transformation(self):
+
         logging.info("Entered initiate data transformation method of data transformation class")
 
         try:
+        # Read data
             dataframe = self.get_data(feature_store_file_path=self.feature_store_file_path)
+
+        # Split features and target
             X = dataframe.drop(columns=TARGET_COLUMN)
             y = np.where(dataframe[TARGET_COLUMN] == -1, 0, 1)
 
+        # Split into training and testing data
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-            
-            preprocessor = self.get_data_transformer_object()
-            X_train_scaled=preprocessor.fit_transform(X_train)
-            X_test_scaled=preprocessor.transform(X_test)
-            
-            preprocessor_path=self.data_transformation_config.transformed_object_file_path
-            os.makrdirs(os.path.dirname(preprocessor_path),exist_ok=True)
-            
-            self.utils.save_object(file_path=preprocessor_path,obj=preprocessor)
-            
-            train_arr=np.c[X_train_scaled,np.array(y_train)]
-            test_arr=np.c[X_test_scaled,np.array(y_test)]
-            
-            return (train_arr, test_arr,preprocessor_path)
-        
-        except Exception as e:
-            raise CustomException(e,sys) from e
-        
-            
 
-        
-        
-        
+        # Get preprocessor object
+            preprocessor = self.get_data_transformer_object(dataframe=X)
+
+        # Transform the data
+            X_train_scaled = preprocessor.fit_transform(X_train)
+            X_test_scaled = preprocessor.transform(X_test)
+
+        # Save the preprocessor
+            preprocessor_path = self.data_transformation_config.transformed_object_file_path
+            os.makedirs(os.path.dirname(preprocessor_path), exist_ok=True)
+            self.utils.save_object(file_path=preprocessor_path, obj=preprocessor)
+
+        # Combine transformed features with target
+            train_arr = np.c_[X_train_scaled, np.array(y_train)]
+            test_arr = np.c_[X_test_scaled, np.array(y_test)]
+
+            return (train_arr, test_arr, preprocessor_path)
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+
 
     
--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -1,11 +1,10 @@
 import sys
+from typing import Dict, Tuple
 import os
 import pandas as pd
 import pickle
 import yaml
-from typing import Dict, Any
 import boto3
-
 
 
 from src.constant import *
@@ -15,68 +14,81 @@ from src.logger import logging
 
 
 
-class Mainutils:
+class MainUtils:
     def __init__(self) -> None:
         pass
 
+
     def read_yaml_file(self, filename: str) -> dict:
-        """
-        Reads a YAML file and returns its contents as a dictionary.
-        """
         try:
-            with open(filename, "r") as yaml_file:  # Corrected mode to 'r'
+            with open(filename, "rb") as yaml_file:
                 return yaml.safe_load(yaml_file)
+
+
         except Exception as e:
             raise CustomException(e, sys) from e
+
 
     def read_schema_config_file(self) -> dict:
-        """
-        Reads the schema configuration YAML file from the 'config' directory.
-        """
         try:
             schema_config = self.read_yaml_file(os.path.join("config", "schema.yaml"))
+
+
             return schema_config
+
+
         except Exception as e:
             raise CustomException(e, sys) from e
 
+
+   
+
+
     @staticmethod
-    def save_objects(file_path: str, obj: object) -> None:
-        """
-        Saves a Python object to a file using pickle.
-        """
-        logging.info("Entered the save_objects method of Mainutils class")
+    def save_object(file_path: str, obj: object) -> None:
+        logging.info("Entered the save_object method of MainUtils class")
+
+
         try:
             with open(file_path, "wb") as file_obj:
                 pickle.dump(obj, file_obj)
-                logging.info("Exited the save_objects method of Mainutils class")
+
+
+            logging.info("Exited the save_object method of MainUtils class")
+
+
         except Exception as e:
             raise CustomException(e, sys) from e
+
+
+   
+
 
     @staticmethod
     def load_object(file_path: str) -> object:
-        """
-        Loads a Python object from a file using pickle.
-        """
-        logging.info("Entered the load_object method of Mainutils class")
+        logging.info("Entered the load_object method of MainUtils class")
+
+
         try:
             with open(file_path, "rb") as file_obj:
                 obj = pickle.load(file_obj)
-                logging.info("Exited the load_object method of Mainutils class")
-                return obj
+
+
+            logging.info("Exited the load_object method of MainUtils class")
+
+
+            return obj
+
+
         except Exception as e:
-            logging.info("Exception occurred in load_object method of Mainutils class")
             raise CustomException(e, sys) from e
-
-
-    @staticmethod
-    def load_object_alt(file_path: str) -> object:
-        """
-        Loads a Python object from a file using pickle (Alternative).
-        """
-        logging.info("Entered the load_object_alt method of Mainutils class")
+   
+    @staticmethod    
+    def load_object(file_path):
         try:
-            with open(file_path, "rb") as file_obj:
+            with open(file_path,'rb') as file_obj:
                 return pickle.load(file_obj)
         except Exception as e:
-            logging.info("Exception occurred in load_object_alt method of Mainutils class")
-            raise CustomException(e, sys) from e
+            logging.info('Exception Occured in load_object function utils')
+            raise CustomException(e,sys)
+   
